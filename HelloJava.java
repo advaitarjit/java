791c94aba2ba60703ad3b392eecf321904c35c43
advaitarjit <145443119+advaitarjit@users.noreply.github.com>
2023-11-14 14:06:05 +0530

    Add files via upload

---
+++ HelloJava.java
@@ -0,0 +1,21 @@
+/*
+ * File         :  HelloJava.java
+ * Description  :  My First Java program that to print "Hellojava"
+ * Author       :  Advait Arjit S
+ * Version      :  1.0
+ * Date         :  19/09/2023
+ */
+
+public class HelloJava{
+
+    public static void main(String [] args) {
+
+    System.out.println("Hello Java");
+
+    System.out.print("Welcome");
+
+    
+
+}
+
+}
