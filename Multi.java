7fdcbd44b8bb7cdcb2784b86964e4654a4201728
advaitarjit <145443119+advaitarjit@users.noreply.github.com>
2023-11-14 14:04:56 +0530

    Add files via upload

---
+++ Multi.java
@@ -0,0 +1,23 @@
+/*
+ * File         :  HelloJava.java
+ * Description  :  Java Program to find product of two Numbers
+ * Author       :  Advait Arjit S
+ * Version      :  1.0
+ * Date         :  19/09/2023
+ */
+
+public class Multi {
+
+	public static void main(String [] args)
+
+	{
+
+		int num1=20,num2=10;
+
+		int mult=num1*num2;
+
+		System.out.print(mult);
+
+	}
+
+}
