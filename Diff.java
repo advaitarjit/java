351256e82604d1017dc7aec5f23f3b378d0569df
advaitarjit <145443119+advaitarjit@users.noreply.github.com>
2023-11-14 14:08:34 +0530

    Add files via upload

---
+++ Diff.java
@@ -0,0 +1,23 @@
+/*
+ * File         :  HelloJava.java
+ * Description  : Java Program to find difference of two numbers
+ * Author       :  Advait Arjit S
+ * Version      :  1.0
+ * Date         :  19/09/2023
+ */
+public class Diff {
+
+	public static void main(String [] args)
+
+	{
+
+		int num1=20,num2=10;
+
+		int diff=num1-num2;
+
+		System.out.print(diff);
+
+	}
+
+
+}
