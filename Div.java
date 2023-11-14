95933cca0641d4854bbb3094ae2b589f85a925ed
advaitarjit <145443119+advaitarjit@users.noreply.github.com>
2023-11-14 14:08:20 +0530

    Add files via upload

---
+++ Div.java
@@ -0,0 +1,21 @@
+/*
+ * File         :  HelloJava.java
+ * Description  :  Java Program to divide two numbers
+ * Author       :  Advait Arjit S
+ * Version      :  1.0
+ * Date         :  19/09/2023
+ */
+public class Div {
+
+	public static void main(String [] args) {
+
+		int num1=10,num2=2;
+
+		int div=num1/num2;
+
+		System.out.print(div);
+
+	}
+
+
+}
