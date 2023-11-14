14f7fab5ab9f1c7deb870cb1e2fe03240f119e13
advaitarjit <145443119+advaitarjit@users.noreply.github.com>
2023-11-14 14:03:23 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,22 @@
+/*
+ * File         :  HelloJava.java
+ * Description  :  Java Program to add two numbers
+ * Author       :  Advait Arjit S
+ * Version      :  1.0
+ * Date         :  19/09/2023
+ */
+
+public class Sum {
+
+	public static void main(String [] args) {
+
+		int num1=10,num2=20;
+
+		int sum=num1+num2;
+
+		System.out.print(sum);
+
+	}
+
+
+}
